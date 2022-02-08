043948425d4156e6471f7f50faf4fdb64fa12124
danielmuthama <daniel.muthama@student.moringaschool.com>
2022-02-09 01:47:24 +0300

    updating final commit from master branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quote_Appl';
+  quotes: Quote[] = [
+    new Quote(
+      Quote.generateId(),
+      "John Keats",
+      "I was never afraid of failure; for I would sooner fail than not be among the greatest.",
+      "James Bond",
+      0,
+      0,
+      new Date('2022,02,08')
+
+
+    ),
+    new Quote(
+      Quote.generateId(),
+      "Harper Lee",
+      "You never really understand a person until you consider things from his point of view. Until you climb inside of his skin and walk around in it.",
+      "Dan curson",
+      0,
+      0,
+      new Date('2022,02,08')
+
+    ),
+    new Quote(
+      Quote.generateId(),
+      "Ernest Hemingway",
+      "There is nothing noble in being superior to your fellow man; true nobility is being superior to your former self.",
+      "Shaddy",
+      0,
+      0,
+      new Date('2022,02,08')
+
+    ),
+  
+
+
+
+
+
+  ]
+
+  newQuote(newQuote: Quote){
+    this.quotes.push(newQuote);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { FormComponent } from './form/form.component';
+import { FormsModule } from '@angular/forms';
+import { ElapsedTimePipe } from './elapsedTime.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuotesComponent,
+    FormComponent,
+    ElapsedTimePipe,
+  
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
